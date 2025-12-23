4dc02096598f47b2f8f738fa4c5605c225d246a5
mugi-tea <moriyuujp@gmail.com>
2025-12-24 00:33:37 +0900

    Add explicit screen and label styling

--- config/boards/shields/oled_test/custom_status_screen.c
+++ config/boards/shields/oled_test/custom_status_screen.c
@@ -4,12 +4,18 @@
  */
 
 #include <lvgl.h>
+#include <zmk/display.h>
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen = lv_obj_create(NULL);
 
+    lv_obj_set_size(screen, 128, 32);
+    lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
+
     lv_obj_t *label = lv_label_create(screen);
     lv_label_set_text(label, "HELLO");
+    lv_obj_set_style_text_color(label, lv_color_white(), LV_PART_MAIN);
     lv_obj_center(label);
 
     return screen;
