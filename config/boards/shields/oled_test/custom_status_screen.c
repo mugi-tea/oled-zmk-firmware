84303c0d3e2c20630b38f582a826ec10d65eb161
mugi-tea <moriyuujp@gmail.com>
2025-12-24 00:17:37 +0900

    Minimal custom screen test - just HELLO label

--- config/boards/shields/oled_test/custom_status_screen.c
+++ config/boards/shields/oled_test/custom_status_screen.c
@@ -3,20 +3,14 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include "widgets/bongo_cat.h"
-
-#include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
-static struct zmk_widget_bongo_cat bongo_cat_widget;
+#include <lvgl.h>
 
 lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen;
+    lv_obj_t *screen = lv_obj_create(NULL);
 
-    screen = lv_obj_create(NULL);
-
-    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_center(zmk_widget_bongo_cat_obj(&bongo_cat_widget));
+    lv_obj_t *label = lv_label_create(screen);
+    lv_label_set_text(label, "HELLO");
+    lv_obj_center(label);
 
     return screen;
 }
