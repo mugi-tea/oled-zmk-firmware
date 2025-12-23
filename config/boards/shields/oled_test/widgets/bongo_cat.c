ba5711ea976deb2e0567d98e1a0103f20c55900e
mugi-tea <moriyuujp@gmail.com>
2025-12-23 23:49:08 +0900

    Use lv_canvas to draw bongo cat pixels directly

--- config/boards/shields/oled_test/widgets/bongo_cat.c
+++ config/boards/shields/oled_test/widgets/bongo_cat.c
@@ -5,142 +5,75 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/bluetooth/services/bas.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
-#include <zmk/event_manager.h>
-#include <zmk/events/wpm_state_changed.h>
-#include <zmk/wpm.h>
 
 #include "bongo_cat.h"
 
-#define SRC(array) (const void **)array, sizeof(array) / sizeof(lv_img_dsc_t *)
-
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-LV_IMG_DECLARE(bongo_cat_none);
-LV_IMG_DECLARE(bongo_cat_left1);
-LV_IMG_DECLARE(bongo_cat_left2);
-LV_IMG_DECLARE(bongo_cat_right1);
-LV_IMG_DECLARE(bongo_cat_right2);
-LV_IMG_DECLARE(bongo_cat_both1);
-LV_IMG_DECLARE(bongo_cat_both1_open);
-LV_IMG_DECLARE(bongo_cat_both2);
+// Image dimensions
+#define IMG_WIDTH 50
+#define IMG_HEIGHT 26
 
-#define ANIMATION_SPEED_IDLE 10000
-const lv_img_dsc_t *idle_imgs[] = {
-    &bongo_cat_both1_open,
-    &bongo_cat_both1_open,
-    &bongo_cat_both1_open,
-    &bongo_cat_both1,
+// Bongo cat bitmap data (1 bit per pixel, 7 bytes per row)
+static const uint8_t bongo_cat_both1_data[] = {
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x02, 0x40, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x04, 0x40, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x18, 0x7c, 0x02, 0x00, 0x00,
+  0x00, 0x00, 0x20, 0x07, 0xc5, 0x00, 0x00,
+  0x00, 0x00, 0x40, 0x00, 0x79, 0x00, 0x00,
+  0x00, 0x00, 0x80, 0x00, 0x11, 0x00, 0x00,
+  0x00, 0x00, 0x80, 0x00, 0x01, 0x00, 0x00,
+  0x00, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00,
+  0x00, 0x01, 0x08, 0x00, 0x02, 0x00, 0x00,
+  0x00, 0x02, 0x14, 0x10, 0x02, 0x00, 0x00,
+  0xe0, 0x04, 0x00, 0x28, 0x02, 0x00, 0x00,
+  0x1f, 0x08, 0x01, 0x00, 0x01, 0x00, 0x00,
+  0x00, 0xf0, 0x03, 0x80, 0x01, 0x80, 0x00,
+  0x00, 0x10, 0x20, 0x00, 0x00, 0x80, 0x00,
+  0x00, 0x10, 0x7f, 0x82, 0x00, 0x80, 0x00,
+  0x00, 0x09, 0x80, 0x7c, 0x00, 0xc0, 0x00,
+  0x00, 0x06, 0x00, 0x08, 0x10, 0x40, 0x00,
+  0x00, 0x00, 0x00, 0x08, 0x2f, 0xe0, 0x00,
+  0x00, 0x00, 0x00, 0x04, 0x40, 0x3e, 0x00,
+  0x00, 0x00, 0x00, 0x03, 0x80, 0x01, 0xc0,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-#define ANIMATION_SPEED_SLOW 2000
-const lv_img_dsc_t *slow_imgs[] = {
-    &bongo_cat_left1,
-    &bongo_cat_both1,
-    &bongo_cat_both1,
-    &bongo_cat_right1,
-    &bongo_cat_both1,
-    &bongo_cat_both1,
-    &bongo_cat_left1,
-    &bongo_cat_both1,
-    &bongo_cat_both1,
-};
+static void draw_bongo_cat(lv_obj_t *canvas) {
+    lv_canvas_fill_bg(canvas, lv_color_black(), LV_OPA_COVER);
 
-#define ANIMATION_SPEED_MID 500
-const lv_img_dsc_t *mid_imgs[] = {
-    &bongo_cat_left2,
-    &bongo_cat_left1,
-    &bongo_cat_none,
-    &bongo_cat_right2,
-    &bongo_cat_right1,
-    &bongo_cat_none,
-};
+    for (int y = 0; y < IMG_HEIGHT; y++) {
+        for (int x = 0; x < IMG_WIDTH; x++) {
+            int byte_idx = y * 7 + (x / 8);
+            int bit_idx = 7 - (x % 8);
 
-#define ANIMATION_SPEED_FAST 200
-const lv_img_dsc_t *fast_imgs[] = {
-    &bongo_cat_both2,
-    &bongo_cat_both1,
-    &bongo_cat_none,
-    &bongo_cat_none,
-};
-
-struct bongo_cat_wpm_status_state {
-    uint8_t wpm;
-};
-
-enum anim_state {
-    anim_state_none,
-    anim_state_idle,
-    anim_state_slow,
-    anim_state_mid,
-    anim_state_fast
-} current_anim_state;
-
-static void set_animation(lv_obj_t *animing, struct bongo_cat_wpm_status_state state) {
-    if (state.wpm < 5) {
-        if (current_anim_state != anim_state_idle) {
-            lv_animimg_set_src(animing, SRC(idle_imgs));
-            lv_animimg_set_duration(animing, ANIMATION_SPEED_IDLE);
-            lv_animimg_set_repeat_count(animing, LV_ANIM_REPEAT_INFINITE);
-            lv_animimg_start(animing);
-            current_anim_state = anim_state_idle;
-        }
-    } else if (state.wpm < 30) {
-        if (current_anim_state != anim_state_slow) {
-            lv_animimg_set_src(animing, SRC(slow_imgs));
-            lv_animimg_set_duration(animing, ANIMATION_SPEED_SLOW);
-            lv_animimg_set_repeat_count(animing, LV_ANIM_REPEAT_INFINITE);
-            lv_animimg_start(animing);
-            current_anim_state = anim_state_slow;
-        }
-    } else if (state.wpm < 70) {
-        if (current_anim_state != anim_state_mid) {
-            lv_animimg_set_src(animing, SRC(mid_imgs));
-            lv_animimg_set_duration(animing, ANIMATION_SPEED_MID);
-            lv_animimg_set_repeat_count(animing, LV_ANIM_REPEAT_INFINITE);
-            lv_animimg_start(animing);
-            current_anim_state = anim_state_mid;
-        }
-    } else {
-        if (current_anim_state != anim_state_fast) {
-            lv_animimg_set_src(animing, SRC(fast_imgs));
-            lv_animimg_set_duration(animing, ANIMATION_SPEED_FAST);
-            lv_animimg_set_repeat_count(animing, LV_ANIM_REPEAT_INFINITE);
-            lv_animimg_start(animing);
-            current_anim_state = anim_state_fast;
+            if (bongo_cat_both1_data[byte_idx] & (1 << bit_idx)) {
+                lv_canvas_set_px(canvas, x, y, lv_color_white(), LV_OPA_COVER);
+            }
         }
     }
 }
 
-struct bongo_cat_wpm_status_state bongo_cat_wpm_status_get_state(const zmk_event_t *eh) {
-    struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(eh);
-    return (struct bongo_cat_wpm_status_state) { .wpm = ev->state };
-};
-
-void bongo_cat_wpm_status_update_cb(struct bongo_cat_wpm_status_state state) {
-    struct zmk_widget_bongo_cat *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_animation(widget->obj, state); }
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_bongo_cat, struct bongo_cat_wpm_status_state,
-                            bongo_cat_wpm_status_update_cb, bongo_cat_wpm_status_get_state)
-
-ZMK_SUBSCRIPTION(widget_bongo_cat, zmk_wpm_state_changed);
-
 int zmk_widget_bongo_cat_init(struct zmk_widget_bongo_cat *widget, lv_obj_t *parent) {
-    // Test with static image first
-    widget->obj = lv_image_create(parent);
-    lv_image_set_src(widget->obj, &bongo_cat_both1);
+    static lv_color_t cbuf[LV_CANVAS_BUF_SIZE(IMG_WIDTH, IMG_HEIGHT, 1, 0)];
+
+    widget->obj = lv_canvas_create(parent);
+    lv_canvas_set_buffer(widget->obj, cbuf, IMG_WIDTH, IMG_HEIGHT, LV_COLOR_FORMAT_L8);
     lv_obj_center(widget->obj);
 
-    sys_slist_append(&widgets, &widget->node);
+    draw_bongo_cat(widget->obj);
 
-    // widget_bongo_cat_init();
+    sys_slist_append(&widgets, &widget->node);
 
     return 0;
 }
